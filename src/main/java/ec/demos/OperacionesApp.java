d880831d911e3931f0916df5e952c951ea865cd8
Pedro Ordoñez <pedroo382@gmail.com>
2022-01-28 21:52:29 -0500

    Agregado: Metodo Suma y Resta

--- src/main/java/ec/demos/OperacionesApp.java
+++ src/main/java/ec/demos/OperacionesApp.java
@@ -3,8 +3,16 @@ package ec.demos;
 public class OperacionesApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Instituto");
+		Operaciones operaciones = new Operaciones ();
+		operaciones.setValue1(50);
+		operaciones.setValue2(83);
+		operaciones.sumar();
 
+		OperResta oper = new OperResta();
+		oper.setValue3(20);
+		oper.setValue4(5);
+		oper.restar();
+		
 	}
 
 }
