527591ca3623d2ea77e8c6375d4e446b93774f46
Pedro Ordoñez <pedroo382@gmail.com>
2022-01-31 16:46:17 -0500

    Added: Operación Calculadora

--- src/main/java/ec/demos/Calculadora.java
+++ src/main/java/ec/demos/Calculadora.java
@@ -5,7 +5,12 @@ public class Calculadora extends Operaciones{
 	public void modulo () {
 		int result = getValue1() % getValue2();
 		System.out.println ("Modulo " + result);
+		
+		
 	}
-
+	public void multiplicacion () {
+		int multi =  getValue1() * getValue2(); 
+		System.out.println("Multiplicación= "+ multi);
+	}
 }
 
